a9c61fc64367c53d726728ad28f87e32ddcf9236
William Luqui <william.luqui@gmail.com>
2022-05-10 00:30:02 -0300

    estacionamento

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,8 @@
 
-
-
 btnRegistro.addEventListener("click", ()=>{
-  
   patioCheio();
-  
 });
+
 /* == HORA == */
 let relogio = () => {
  let data = new Date(); // obrigatorio para puxar a hora
@@ -17,10 +14,10 @@ let relogio = () => {
   }
   if (min < 10) {
     min = "0" + min;
-  }
+  }  
   return `${hora}:${min}`
+  
 }
-
 function entradaVeiculos(){
     let placa = document.getElementById("placa").value;
     let modelo = document.getElementById("modelo").value;
@@ -35,45 +32,56 @@ function entradaVeiculos(){
       sucesso.style.background = 'rgb(247, 91, 91)';
       return false;
     }
-      
-    
-    
-  carro = {
-    modelo:modelo,
-    placa:placa,
-    hora:relogio(),
-   
+     carro = {
+      modelo:modelo,
+      placa:placa,
+      hora:relogio(),    
   }
+  
   if(localStorage.getItem('patio') === null){
-    let carros =[];
+    let carros = [];
     carros.push(carro);
     localStorage.setItem('patio', JSON.stringify(carros));
-  
-  }
-    
+    document.getElementById('cadastro').reset();
+    sucesso.style.background = 'rgb(86, 216, 81)';
+    sucesso.innerHTML = 'Adicionado com Sucesso !';
+
+   }else{
     let carros = JSON.parse(localStorage.getItem('patio'));
     carros.push(carro);
+    
     localStorage.setItem('patio', JSON.stringify(carros));
+    carros ++;
+    console.log(` teste ${carros.length}`)
     sucesso.innerHTML = 'Adicionado com Sucesso !';
     sucesso.style.background = 'rgb(86, 216, 81)';
     document.getElementById('cadastro').reset(); 
-  
   }
   
+  
+  
+  
+  
+  
+}
+
+
+
+
 function apagarVeiculo(placa){
 let carros = JSON.parse(localStorage.getItem('patio'));
  for(let i = 0; i < carros.length; i++){
    if(carros[i].placa == placa){
-     carros.splice(i, 1); // splice remove
-     
+     carros.splice(i, 1); // splice= remove
    }
-   
    localStorage.setItem('patio',JSON.stringify(carros));
    remover.innerHTML = 'Carro removido do Patío!';
    remover.style.background = 'rgb(247, 91, 91)';
  }
-mostraPatio()
-}    
+ 
+mostraPatio();
+}   
+
 function mostraPatio(){
   let carros = JSON.parse( localStorage.getItem('patio'));
   let carrosResultado = document.getElementById('resultados');
@@ -82,45 +90,31 @@ function mostraPatio(){
     let modelo = carros[i].modelo;
     let placa = carros[i].placa;
     let hora = carros[i].hora;
-    carrosResultado.innerHTML += '<tr><td>' + placa +
+    
+    carrosResultado.innerHTML += '<tr>' +
+                                    '<td>' + placa +
                                    '</td><td>'+ modelo +
                                    '</td><td>' + hora +
                                    '</td><td><button class="finalizar" onclick="apagarVeiculo(\''+placa+'\')">Excluir</button></td>' +
                                    '</tr>';
      }
+     
+     
 }
 
+
 function patioCheio(){
-  let vagas = 10;
+  let vagasTotais = 10;
   let carros = JSON.parse( localStorage.getItem('patio'));
-  if( carros.length + 1 > vagas){
+  if (localStorage.getItem('patio') === null){
+    let carros = [];
+    localStorage.setItem('patio', JSON.stringify(carros));
+  }else if( carros.length + 1 > vagasTotais){
     sucesso.innerHTML = 'Estacionamento cheio ! <br> Limite de 10 Vagas atingidas ';
     sucesso.style.background = 'rgb(247, 91, 91)';
     return false;
-    
   }
+
+  
   entradaVeiculos();
 }
-
-// function validarPlaca(placa){
- 
-//   const regexPlaca = /^[a-zA-Z]{3}[0-9]{4}$/;
-//   const regexPlacaMercosulCarro = /^[a-zA-Z]{3}[0-9]{1}[a-zA-Z]{1}[0-9]{2}$/;
-//   const regexPlacaMercosulMoto = /^[a-zA-Z]{3}[0-9]{2}[a-zA-Z]{1}[0-9]{1}$/;
-//   if(regexPlaca.test(placa)){
-//   resposta = "Placa válida no formato atual";
-//   }
-//   if(regexPlacaMercosulCarro.test(placa)){
-//   resposta = "Placa válida (padrão Mercosul - carro)";
-//   }
-//   if(regexPlacaMercosulMoto.test(placa)){
-//   resposta = "Placa válida (padrão Mercosul - moto)";
-//   }
-//   return resposta;
-//  }
-//  console.log("teste");
-//  console.log(validarPlaca("ABC1234"));
-//  console.log(validarPlaca("ABC12356"));
-//  console.log(validarPlaca("ABC12D5"));
-//  console.log(validarPlaca("ABC1D15"));
-//  console.log(validarPlaca("ABC12D51"));
